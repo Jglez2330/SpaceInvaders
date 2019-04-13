09eb5a9786b8303ea52d1a64c2e2a6ac9ebee318
JoseChV <36898484+JoseChV@users.noreply.github.com>
2019-04-12 22:44:04 -0600

    .

---
+++ cmake-build-debug/CMakeFiles/SpaceInvaders.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jose/CLionProjects/SpaceInvaders/InvaderHandler.c" "/home/jose/CLionProjects/SpaceInvaders/cmake-build-debug/CMakeFiles/SpaceInvaders.dir/InvaderHandler.c.o"
+  "/home/jose/CLionProjects/SpaceInvaders/bunker.c" "/home/jose/CLionProjects/SpaceInvaders/cmake-build-debug/CMakeFiles/SpaceInvaders.dir/bunker.c.o"
+  "/home/jose/CLionProjects/SpaceInvaders/invader.c" "/home/jose/CLionProjects/SpaceInvaders/cmake-build-debug/CMakeFiles/SpaceInvaders.dir/invader.c.o"
+  "/home/jose/CLionProjects/SpaceInvaders/linkedList.c" "/home/jose/CLionProjects/SpaceInvaders/cmake-build-debug/CMakeFiles/SpaceInvaders.dir/linkedList.c.o"
+  "/home/jose/CLionProjects/SpaceInvaders/main.c" "/home/jose/CLionProjects/SpaceInvaders/cmake-build-debug/CMakeFiles/SpaceInvaders.dir/main.c.o"
+  "/home/jose/CLionProjects/SpaceInvaders/misil.c" "/home/jose/CLionProjects/SpaceInvaders/cmake-build-debug/CMakeFiles/SpaceInvaders.dir/misil.c.o"
+  "/home/jose/CLionProjects/SpaceInvaders/nave.c" "/home/jose/CLionProjects/SpaceInvaders/cmake-build-debug/CMakeFiles/SpaceInvaders.dir/nave.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SpaceInvaders.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceInvaders.dir/main.c.o"
+  "CMakeFiles/SpaceInvaders.dir/invader.c.o"
+  "CMakeFiles/SpaceInvaders.dir/linkedList.c.o"
+  "CMakeFiles/SpaceInvaders.dir/InvaderHandler.c.o"
+  "CMakeFiles/SpaceInvaders.dir/misil.c.o"
+  "CMakeFiles/SpaceInvaders.dir/nave.c.o"
+  "CMakeFiles/SpaceInvaders.dir/bunker.c.o"
+  "bin/SpaceInvaders.pdb"
+  "bin/SpaceInvaders"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SpaceInvaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
